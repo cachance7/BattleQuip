3cbdbecc85e66af00860ba33a4c8dab83a9b0939
Casey Chance <caseyjchance@gmail.com>
2014-07-22 22:39:57 -0400

    Restructure for packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='BattleQuip',
+    version='0.1.0',
+    author='Casey Chance',
+    author_email='caseyjchance@gmail.com',
+    packages=['battlequip', 'battlequip.test'],
+    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/BattleQuip/',
+    license='LICENSE.txt',
+    description='A chatty Battleship client.',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
